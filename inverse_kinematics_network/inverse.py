6713458ae58778699b663e458761562fa9f13d88
ASmallSquishySquid <phoebescaccia@gmail.com>
2021-06-10 02:13:59 -0700

    Relu activation function works better

--- inverse_kinematics_network/inverse.py
+++ inverse_kinematics_network/inverse.py
@@ -49,7 +49,7 @@ features = data.iloc[:, 7:14]
 features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.25)
 
 model = create_model()
-history = fit_model(model, features_train, labels_train, 3000, 512)
+history = fit_model(model, features_train, labels_train, 500, 512)
 plot(history, "inverse_kinematics_network/model_history.png")
 val_mse, val_mae = model.evaluate(features_test, labels_test, verbose = 0)
 print(val_mse, val_mae)
