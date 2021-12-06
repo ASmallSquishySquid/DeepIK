e97ecb388512bc2133a891ff4c016a528ab12ccb
ASmallSquishySquid <phoebescaccia@gmail.com>
2021-12-06 10:22:09 -0600

    Fix error where length could be negative

--- Manual Data Generation/functions.py
+++ Manual Data Generation/functions.py
@@ -8,8 +8,8 @@ def dependent(p1, p2, p3, length):
 	theta2 = math.radians(f(p2))
 	theta3 = math.radians(f(p3))
 	length1 = length / 4
-	length2 = min([length - length1, length / (4 * math.cos(theta1))])
-	length3 = min([length - length1 - length2, length / (4 * math.cos(theta1 + theta2))])
+	length2 = min([length - length1, abs(length / (4 * math.cos(theta1)))])
+	length3 = min([length - length1 - length2, abs(length / (4 * math.cos(theta1 + theta2)))])
 	length4 = max([0, length - length1 - length2 - length3])
 	return theta1, theta2, theta3, length1, length2, length3, length4
 
