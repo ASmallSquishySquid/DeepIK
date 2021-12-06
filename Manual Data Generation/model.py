75235b5eafbeb28fdec76795606f6c1bae858028
ASmallSquishySquid <phoebescaccia@gmail.com>
2021-12-06 11:10:31 -0600

    Train and test on randomized "in-between" powers

--- Manual Data Generation/model.py
+++ Manual Data Generation/model.py
@@ -26,7 +26,7 @@ def plot(history, path):
 def create_model():
 	model = Sequential()
 	# input layer
-	model.add(InputLayer(input_shape=(3,)))
+	model.add(InputLayer(input_shape=(2,)))
 	# hidden layers
 	model.add(Dense(128, activation = "relu"))
 	model.add(Dropout(rate = 0.05))
@@ -34,7 +34,7 @@ def create_model():
 	model.add(Dropout(rate = 0.05))
 	model.add(Dense(16, activation = "relu"))
 	# output layer
-	model.add(Dense(2))
+	model.add(Dense(3))
 	model.compile(optimizer = "adam", loss='mse',  metrics=['mae'])
 	return model
 
@@ -44,17 +44,18 @@ def fit_model(model, features, labels, epochs, batch_size):
 	return history
 
 data = pd.read_csv("Manual Data Generation\generated_data.csv", header=None, delimiter=",")
-features_train = data.iloc[:-40, 0:3]
-labels_train = data.iloc[:-40, 3:]
-features_test = data.iloc[-40:, 0:3]
-labels_test = data.iloc[-40:, 3:]
+labels_train = data.iloc[:-40, 0:3]
+features_train = data.iloc[:-40, 3:]
+labels_test = data.iloc[-40:, 0:3]
+features_test = data.iloc[-40:, 3:]
 # features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.25)
 
+# train the inverse model
 model = create_model()
-history = fit_model(model, features_train, labels_train, 1000, 50)
+history = fit_model(model, features_train, labels_train, 1500, 50)
 plot(history, "Manual Data Generation/model_history.png")
 val_mse, val_mae = model.evaluate(features_test, labels_test, verbose = 0)
 print(val_mse, val_mae)
 
 model.save("Manual Data Generation/model")
-# 0.17701222002506256 0.18400001525878906
+# 0.5643492937088013 0.6222490072250366
