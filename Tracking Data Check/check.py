8ca91846e01b8197afbf8430921aae7461ad0a45
ASmallSquishySquid <phoebescaccia@gmail.com>
2022-01-11 15:19:22 -0800

    Plot tracking data

--- Tracking Data Check/check.py
+++ Tracking Data Check/check.py
@@ -1,6 +1,15 @@
 import joblib
+import matplotlib.pyplot as plt
+import numpy as np
+import random
 
 with open("Tracking Data Check/pos10t5.pkl", "rb") as file:
 	data = joblib.load(file)
-print(data["default"][0])
 
+img = random.randrange(729)
+check = np.array(data["default"][img])
+x, y = check.T
+
+plt.plot(x, y)
+plt.show()
+plt.savefig("Tracking Data Check/check{img}.png".format(img = img))
