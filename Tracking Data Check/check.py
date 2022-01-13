b7dc8039010db0bdd62e8a475aeb969037b91801
ASmallSquishySquid <phoebescaccia@gmail.com>
2022-01-13 15:16:43 -0800

    Overlay the plots over their corresponding images

--- Tracking Data Check/check.py
+++ Tracking Data Check/check.py
@@ -3,22 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-# plot 10 random ones from each file
-for f in range(10, 16):
-	with open("Tracking Data Check/Run {f}/pos{f}t5.pkl".format(f = f), "rb") as file:
-		data = joblib.load(file)
+# # plot 10 random ones from each file
+# for f in range(10, 16):
+# 	with open("Tracking Data Check/Run {f}/pos{f}t5.pkl".format(f = f), "rb") as file:
+# 		data = joblib.load(file)
 
-	for i in range(10):
-		img = random.randrange(729)
-		check = np.array(data["default"][img])
-		x, y = check.T
+# 	for i in range(10):
+# 		img = random.randrange(729)
+# 		check = np.array(data["default"][img])
+# 		x, y = check.T
 
-		plt.plot(x, y)
-		plt.axis("equal")
-		plt.gca().invert_yaxis()
-		# only 12 and 15 are numbered properly
-		plt.savefig("Tracking Data Check/Run {f}/check{img}.png".format(img = img if (f != 12 and f != 15) else img + 1, f = f))
-		plt.cla()
+# 		plt.plot(x, y)
+# 		plt.axis("equal")
+# 		plt.gca().invert_yaxis()
+# 		# only 12 and 15 are numbered properly
+# 		plt.savefig("Tracking Data Check/Run {f}/check{img}.png".format(img = img if (f != 12 and f != 15) else img + 1, f = f))
+# 		plt.cla()
 
 # check for forks
 # f = 15
@@ -35,3 +35,17 @@ for f in range(10, 16):
 # plt.gca().invert_yaxis()
 # plt.legend(["2", "4", "6", "8"])
 # plt.savefig("Tracking Data Check/Forks/check.png")
+
+# plot over background
+f = 10
+with open("Tracking Data Check/Run {f}/pos{f}t5.pkl".format(f = f), "rb") as file:
+	data = joblib.load(file)
+
+for img in [82, 161, 244, 348, 375, 405, 433, 531, 539, 665]:
+	check = np.array(data["default"][img])
+	x, y = check.T
+	plt.scatter(x, y, s = 0.05)
+	back = plt.imread("Tracking Data Check\Run {f}\pat{img}t=5.bmp".format(f = f, img = img))
+	plt.imshow(back)
+	plt.savefig("Tracking Data Check\Run {f}\overlay_{img}.png".format(f = f, img = img))
+	plt.cla()
