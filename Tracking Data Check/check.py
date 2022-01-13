cff5665227f4b9cdc2a7ea04d00d9fd5d8e35998
ASmallSquishySquid <phoebescaccia@gmail.com>
2022-01-12 16:39:08 -0800

    Create organizational folders

--- Tracking Data Check/check.py
+++ Tracking Data Check/check.py
@@ -3,15 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-with open("Tracking Data Check/pos10t5.pkl", "rb") as file:
-	data = joblib.load(file)
+for f in range(10, 16):
+	with open("Tracking Data Check/Run {f}/pos{f}t5.pkl".format(f = f), "rb") as file:
+		data = joblib.load(file)
 
-for i in range(4):
-	img = random.randrange(729)
-	check = np.array(data["default"][img])
-	x, y = check.T
+	for i in range(10):
+		img = random.randrange(729)
+		check = np.array(data["default"][img])
+		x, y = check.T
 
-	plt.plot(x, y)
-	plt.axis("equal")
-	plt.savefig("Tracking Data Check/check{img}.png".format(img = img))
-	plt.show()
+		plt.plot(x, y)
+		plt.axis("equal")
+		plt.gca().invert_yaxis()
+		plt.savefig("Tracking Data Check/Run {f}/check{img}.png".format(img = img, f = f))
+		plt.cla()
