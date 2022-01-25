290dae4259ce6db155d4e69afaa9de150803d9d1
ASmallSquishySquid <phoebescaccia@gmail.com>
2022-01-25 11:56:22 -0600

    Reorganize files

--- Matlab Data/Exploration.m
+++
@@ -1,8 +0,0 @@
-matObj = matfile('holo3pat3pow.mat'); %reference to the file
-
-disp(matObj.holo_3Pat3Pow(1,:));
-
-% for i = 1:3 %up to 3 for testing purposes, but to use need up to 729
-%     p_holo(i) = matObj.holo_3Pat3Pow(i,:); %point to the column within the variable within the file of interest
-%     disp(p_holo) %for monitoring progress; on lab computer each i was taking ~20 seconds
-% end
--- RNG and Cropping/rngesus.m
+++
@@ -1,8 +0,0 @@
-function [random_num] = rngesus(matr)
-%RNGesus Generates a random row number to use.
-%   Returns a random number between 1 and the number of rows in a given
-%   matrix. In order to pull a random pattern. This responds to changes in
-%   the given matrix's size.
-rows = size(matr, 1);
-random_num = randi(rows, 1);
-end
