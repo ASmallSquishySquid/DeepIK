a4f207af93aaeed67928a227eb74596f28989b49
ASmallSquishySquid <phoebescaccia@gmail.com>
2022-03-03 17:39:50 -0600

    Check if the user wants to delete files and run

--- Spring 2022/Convolutional Model/run.py
+++ Spring 2022/Convolutional Model/run.py
@@ -1,19 +1,37 @@
 from keras.models import load_model
 from PIL import Image
 import numpy as np
-import pandas as pd
-import shutil
-from matplotlib import pyplot as plt
+import ctypes
+import os
 
-exec(open("paint_gui.py").read())
+def check():
+	confirm = ctypes.windll.user32.MessageBoxW(0, "Performing this action will write over results.csv and all images " + 
+		"in generated images. Are you sure you want to do this?", "Are you sure?", 1)
+	if confirm == 1:
+		for file in os.scandir("generated_images"):
+			os.remove(file.path)
+		return;
+	else:
+		quit()
 
 def run_test_images(model):
-	data = np.array([np.array(Image.open("generated_images\generated_{i}.bmp".format(i = i))) for i in range(1, 6)])
+	data = np.array([np.array(Image.open(file.path)) for file in os.scandir("generated_images")])
 	data = data.reshape((data.shape[0], 20, 20, 1))
 	data = data.astype('float32')
 	data = data / 255.0
 
 	model = load_model(model)
-	print(model.predict(data))
+	return model.predict(data)
 
-run_test_images("model_full_data")
+def output_results(results):
+	print("\nLaser outputs:")
+	for result in results:
+		print(result)
+	np.savetxt("results.csv", results, delimiter=",")
+	ctypes.windll.user32.MessageBoxW(0, "Laser patterns have been output to the terminal and results.csv", "Confirmation", 0)
+
+if __name__ == "__main__":
+	check()
+	exec(open("paint_gui.py").read())
+	results = run_test_images("model")
+	output_results(results)
