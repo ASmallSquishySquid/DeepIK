8aaf3f5408d5697379663133c2f07b1031445183
ASmallSquishySquid <phoebescaccia@gmail.com>
2022-03-04 12:16:37 -0600

    Add model name prompt

--- Spring 2022/Convolutional Model/control/run.py
+++ Spring 2022/Convolutional Model/control/run.py
@@ -1,15 +1,21 @@
-from keras.models import load_model
-from PIL import Image
 import numpy as np
 import ctypes
 import os
+from tkinter import *
+from keras.models import load_model
+from PIL import Image
+from prompt_toolkit import prompt
 
 def check():
 	confirm = ctypes.windll.user32.MessageBoxW(0, "Performing this action will write over results.csv and all images " + 
 		"in generated images. Are you sure you want to do this?", "Are you sure?", 1)
 	if confirm == 1:
+		print("Removing old images from generated_images...")
 		for file in os.scandir("generated_images"):
 			os.remove(file.path)
+		print("Removing results.csv...")
+		os.remove("results.csv")
+		print("Removed.")
 		return;
 	else:
 		quit()
@@ -30,10 +36,27 @@ def output_results(results):
 	np.savetxt("results.csv", results, delimiter=",")
 	ctypes.windll.user32.MessageBoxW(0, "Laser patterns have been output to the terminal and results.csv", "Confirmation", 0)
 
+class ModelPrompt:
+	def __init__(self):
+		self.prompt = Tk()
+		self.L1 = Label(self.prompt, text="Model name").grid(row=0, column=0)
+		self.E1 = Entry(self.prompt, bd =5)
+		self.E1.grid(row=0, column=2)
+		self.prompt.bind("<Return>", self.save_model)
+		self.button1 = Button(self.prompt, text="Enter", command=self.save_model).grid(row=1, column=1)
+		self.prompt.mainloop()
+
+	def get_model(self):
+		return self.model
+
+	def save_model(self, event=None):
+		self.model = self.E1.get()
+		self.prompt.destroy()
+
 if __name__ == "__main__":
 	check()
 	exec(open("paint_gui.py").read())
-	print("Enter model name:")
-	model = input()
+	prompt = ModelPrompt()
+	model = prompt.get_model()
 	results = run_test_images(model)
 	output_results(results)
