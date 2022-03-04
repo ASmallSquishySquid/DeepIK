e284bd99d7b0f2bf73ede837e2c18230fd94496e
ASmallSquishySquid <phoebescaccia@gmail.com>
2022-03-04 12:04:03 -0600

    Prompt user to input a model and folder

--- Spring 2022/Convolutional Model/control/run_existing_images.py
+++ Spring 2022/Convolutional Model/control/run_existing_images.py
@@ -1,47 +1,57 @@
 from keras.models import load_model
-from PIL import Image
 import numpy as np
 import ctypes
 import os
 from run import output_results
 from tkinter import *
+from PIL import Image
 
-model = "";
-folder = "";
+class VariableGetter:
+	def __init__(self):
+		self.top = Tk()
+		self.L1 = Label(self.top, text="Model name").grid(row=0, column=0)
+		self.E1 = Entry(self.top, bd =5)
+		self.E1.grid(row=0, column=2)
+		self.L2 = Label(self.top, text="Folder").grid(row=1, column=0)
+		self.E2 = Entry(self.top, bd =5)
+		self.E2.grid(row=1, column=2)
+		self.top.bind("<Return>", self.get_vars_button)
+		self.button1 = Button(self.top, text="Enter", command=self.get_vars_button).grid(row=3, column=1)
+		self.top.mainloop()
+
+	def get_model(self):
+		return self.model
+	
+	def get_folder(self):
+		return self.folder
+
+	def get_vars_button(self, event=None):
+		self.model = self.E1.get()
+		self.folder = self.E2.get()
+		self.top.destroy()
 
 def confirm():
 	confirm = ctypes.windll.user32.MessageBoxW(0, "Performing this action will write over results.csv. Are you sure you want to do this?", "Are you sure?", 1)
 	if confirm != 1:
 		quit()
 
-def get_vars():
-	top = Tk()
-	L1 = Label(top, text="Model name")
-	L1.pack( side = LEFT)
-	E1 = Entry(top, bd =5)
-	E1.pack(side = RIGHT)
-	L2 = Label(top, text="Folder")
-	L2.pack( side = LEFT)
-	E2 = Entry(top, bd =5)
-	E2.pack(side = RIGHT)
-	button1 = Button(top, text="Enter", command=get_vars_helper)
-	button1.pack()
-	top.mainloop()
-
-def get_vars_helper():
-	pass
-
-def run_test_images(model):
+def run_test_images(model, folder):
 	print("\nProcessing...")
-	data = np.array([np.array(Image.open(file.path)) for file in os.scandir(folder)])
+	data = np.array([np.array(Image.open(file.path)) for file in os.scandir(folder) if file.name.endswith(".bmp") and file.is_file()])
 	data = data.reshape((data.shape[0], 20, 20, 1))
 	data = data.astype('float32')
 	data = data / 255.0
+
+	if len(data) == 0:
+		ctypes.windll.user32.MessageBoxW(0, "There are no .bmp files in this folder", "No valid files", 0)
+		print("Program terminated")
+		quit()
+
 	model = load_model(model)
 	return model.predict(data)
 
 if __name__ == "__main__":
 	confirm()
-	get_vars()
-	results = run_test_images(model)
+	vars = VariableGetter()
+	results = run_test_images(vars.get_model(), vars.get_folder())
 	output_results(results)
