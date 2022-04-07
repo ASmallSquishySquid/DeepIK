64c1af38493994b9fc6b5db264340eacccc49734
ASmallSquishySquid <phoebescaccia@gmail.com>
2022-04-07 02:24:51 -0500

    Change drawing to match new sample

--- Spring 2022/Convolutional Model/control/resources/modules/paint_gui.py
+++ Spring 2022/Convolutional Model/control/resources/modules/paint_gui.py
@@ -26,26 +26,26 @@ class ImageDrawing:
 		parent.iconbitmap("resources/icons/paint.ico")
 		self.b1 = "up"
 		self.xold, self.yold = None, None
-		self.drawing_area = Canvas(parent, width=200, height=200, bg="black")
+		self.drawing_area = Canvas(parent, width=350, height=350, bg="black")
 		self.drawing_area.grid(row=0, column=0, columnspan=3)
 		self.drawing_area.bind("<Motion>", self.motion)
 		self.drawing_area.bind("<ButtonPress-1>", self.b1down)
 		self.drawing_area.bind("<ButtonRelease-1>", self.b1up)
-		self.drawing_area.create_rectangle(95, 0, 105, 5, fill="white")
+		self.drawing_area.create_rectangle(170, 0, 180, 5, fill="white")
 		Button(parent, text="Clear", command=self.clear_canvas, background="IndianRed1").grid(row=1, column=0, pady=2)
 		Button(parent, text="Save", command=self.save, background="PaleGreen1").grid(row=1, column=1, pady=2)
 		Button(parent, text="Done", command=parent.destroy, background="SkyBlue1").grid(row=1, column=2, pady=2)
 		parent.bind("<Return>", self.save)
 		parent.bind("<BackSpace>", self.clear_canvas)
-		self.image = Image.new("1",(200,200))
+		self.image = Image.new("1",(350,350))
 		self.draw = ImageDraw.Draw(self.image)
 		parent.mainloop()
 
 	def clear_canvas(self):
 		self.drawing_area.delete("all")
-		self.image=Image.new("1",(200,200))
+		self.image=Image.new("1",(350, 350))
 		self.draw=ImageDraw.Draw(self.image)
-		self.drawing_area.create_rectangle(95, 0, 105, 5, fill="white")
+		self.drawing_area.create_rectangle(170, 0, 180, 5, fill="white")
 
 	def b1down(self, event):
 		self.b1 = "down"
@@ -59,12 +59,12 @@ class ImageDrawing:
 		if self.b1 == "down":
 			if self.xold is not None and self.yold is not None:
 				event.widget.create_line(self.xold, self.yold, event.x,event.y,smooth=TRUE, width=10, fill="white")
-				self.draw.line(((self.xold, self.yold), (event.x,event.y)), 1, width=20)
+				self.draw.line(((self.xold, self.yold), (event.x,event.y)), 1, width=10)
 			self.xold = event.x
 			self.yold = event.y
 
-	def save(self):
-		self.image.convert("L").resize((20, 20)).save("{folder_name}\generated_{i}.bmp".format(i = self.counter, folder_name = self.folder))
+	def save(self, event=None):
+		self.image.convert("L").resize((35, 35)).save("{folder_name}\generated_{i}.bmp".format(i = self.counter, folder_name = self.folder))
 		messagebox.showinfo("Image saved", "Saved as generated_{i}.bmp in folder {folder_name}".format(i = self.counter, folder_name = self.folder))
 		self.counter += 1
 		self.clear_canvas()
@@ -90,7 +90,6 @@ class ImageDrawing:
 				self.count["bg"] = "misty rose"
 			else:
 				self.count["bg"] = "snow"
-
 
 	def get_folder(self):
 		return self.folder
